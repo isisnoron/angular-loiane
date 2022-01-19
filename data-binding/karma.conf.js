7db6bfa877216209e9c788e2d968e3b2823870b6
Isis Beatriz Ribeiro de Noronha <ibnoronha@rd.com.br>
2022-01-19 13:46:42 -0300

    diretiva ngswitch

--- data-binding/karma.conf.js
+++ data-binding/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/data-biding'),
+      dir: require('path').join(__dirname, './coverage/data-binding'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
+import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DiretivaNgifComponent
+    DiretivaNgifComponent,
+    DiretivaNgswitchComponent
   ],
   imports: [
     BrowserModule
