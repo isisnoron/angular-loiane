7c43064d434af57eae03d483268aa030583a9783
Isis Beatriz Ribeiro de Noronha <ibnoronha@rd.com.br>
2022-01-20 15:59:25 -0300

    injeção de dependencia: como usar serviço em um comp

--- servicos/src/app/cursos/cursos.component.ts
+++ servicos/src/app/cursos/cursos.component.ts
@@ -12,8 +12,9 @@ export class CursosComponent implements OnInit {
   cursos: string[] = [];
   cursosService: CursosService;
 
-  constructor() {
-    this.cursosService = new CursosService();
+  constructor(private _cursosService: CursosService) {
+    // this.cursosService = new CursosService();
+    this.cursosService = _cursosService;
 
   }
 
--- servicos/src/app/cursos/cursos.service.ts
+++ servicos/src/app/cursos/cursos.service.ts
@@ -1,5 +1,9 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({ providedIn: "root" })
+
 export class CursosService{
-    
+
     getCursos() {
         return ['Angular', 'Java', 'Phonegap'];
     }
