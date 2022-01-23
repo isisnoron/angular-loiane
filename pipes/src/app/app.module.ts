66efb638c9433abc07852466e68d071233a5f934
Isis Beatriz Ribeiro de Noronha <isisbeatrizrn@gmail.com>
2022-01-23 15:54:06 -0300

    criando pipe impuro (filtro)

--- pipes/src/app/app.module.ts
+++ pipes/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { CamelCasePipe } from './camel-case.pipe';
 import { SettingsService } from './settings.service';
 import { FiltroArrayPipe } from './filtro-array.pipe';
 import { FormsModule } from '@angular/forms';
+import { FiltroArrayImpuroPipe } from './filtro-array-impuro.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     ExemplosPipesComponent,
     CamelCasePipe,
-    FiltroArrayPipe
+    FiltroArrayPipe,
+    FiltroArrayImpuroPipe
   ],
   imports: [
     BrowserModule,
--- pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
+++ pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -22,6 +22,19 @@ export class ExemplosPipesComponent implements OnInit {
     this.livros.push(curso)
   }
 
+  obterCursos(){
+    if (this.livros.length === 0 || this.filtro === undefined || this.filtro.trim() === ''){
+      return this.livros;
+    }
+
+    return this.livros.filter((v) => {
+      if (v.toLowerCase().indexOf(this.filtro.toLowerCase()) >= 0) {
+        return true;
+        }
+   return false;
+   })
+  }
+
   constructor() { }
  
   ngOnInit(): void {
