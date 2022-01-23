d80d82adcee8cb9e3d5fa56ab4ff19154d8fe60f
Isis Beatriz Ribeiro de Noronha <isisbeatrizrn@gmail.com>
2022-01-23 17:56:46 -0300

    routerLink definindo rotas e usando material

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatSliderModule } from '@angular/material/slider';
+import { MatIconModule} from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
@@ -18,7 +24,14 @@ import { routing } from './app.routing';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    routing
+    routing,
+    MatSidenavModule,
+    MatSliderModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
