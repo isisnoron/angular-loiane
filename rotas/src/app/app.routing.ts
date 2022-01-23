9efeb2cbdfb7e89316681683ae97e7144a5e6476
Isis Beatriz Ribeiro de Noronha <isisbeatrizrn@gmail.com>
2022-01-23 18:30:42 -0300

    obtendo parametro da rota (1ª forma)

--- rotas/src/app/app.routing.ts
+++ rotas/src/app/app.routing.ts
@@ -4,11 +4,13 @@ import { ModuleWithProviders } from '@angular/core';
 import { CursosComponent } from './cursos/cursos.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
 
 
 const APP_ROUTES: Routes = [
     {path: 'cursos', component: CursosComponent},
+    {path: 'curso/:id', component: CursoDetalheComponent},
     {path: 'login', component : LoginComponent},
     {path: '', component: HomeComponent}
 
