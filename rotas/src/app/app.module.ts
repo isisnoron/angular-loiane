90e639522e3f5d0366c5bbaa708ca7082fbfe69a
Isis Beatriz Ribeiro de Noronha <isisbeatrizrn@gmail.com>
2022-01-23 16:54:27 -0300

    criando componentes

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    CursosComponent
   ],
   imports: [
     BrowserModule,
