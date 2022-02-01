a9794d4432f3b891be05a767dd4660d892b8d1eb
Isis Ribeiro <isisbeatrizrn@gmail.com>
2022-02-01 14:23:02 -0300

    aplicando css na validação dos campos

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class TemplateFormComponent implements OnInit {
 
   usuario:any = {
-    nome: 'Isis',
-    email: 'isis@email.com'
+    nome: null,
+    email: null
   }
 
   onSubmit(form:any) {
