8ed73f481d1b3c621dfb45ff983ab949c05f7247
Isis Ribeiro <isisbeatrizrn@gmail.com>
2022-02-01 13:27:53 -0300

    Controles ngForm, ngSubmit e ngModel

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateFormComponent implements OnInit {
 
+  onSubmit(form:any) {
+    console.log(form);
+  }
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
