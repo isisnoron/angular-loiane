d4cf7550b07535c580918a151caadc4fb3ce9308
Isis Ribeiro <isisbeatrizrn@gmail.com>
2022-02-04 13:59:48 -0300

    agrupando dados - ngModelGroup

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -30,5 +30,14 @@ export class TemplateFormComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  // verificaValidTouched(campo:any){
+  //   return !campo.valid && campo.touched
+  // }
+
+  // aplicaCssErro(campo:any){
+  //   return !campo.valid && campo.touched
+ 
+  // }
+
 
 }
