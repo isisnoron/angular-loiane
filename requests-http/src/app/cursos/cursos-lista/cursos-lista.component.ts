220da7746c1dc914ea14cac378d28e322e3a0683
Isis Ribeiro <isisbeatrizrn@gmail.com>
2022-02-28 13:48:27 -0300

    Serviço de alert genérico com bootstrap

--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -4,6 +4,7 @@ import { CursosService } from '../cursos.service';
 import { catchError, empty, Observable, Subject, switchMap } from 'rxjs';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { AlertModalComponent } from '../../shared/alert-modal/alert-modal.component';
+import { AlertModalService } from 'src/app/shared/alert-modal.service';
 
 @Component({
   selector: 'app-cursos-lista',
@@ -14,14 +15,15 @@ import { AlertModalComponent } from '../../shared/alert-modal/alert-modal.compon
 
 export class CursosListaComponent implements OnInit {
 
-  bsModalRef: BsModalRef;
+  // bsModalRef: BsModalRef;
 
   // cursos: Curso[];
   cursos$: Observable<Curso[]>;
   error$ = new Subject<boolean>();
 
   constructor(private service: CursosService,
-    private modalService: BsModalService) { }
+    //private modalService: BsModalService
+    private alertService: AlertModalService) { }
 
   ngOnInit(): void {
     this.onRefresh();
@@ -52,9 +54,10 @@ export class CursosListaComponent implements OnInit {
   }
 
   handleError() {
-    this.bsModalRef = this.modalService.show(AlertModalComponent);
-    this.bsModalRef.content.type = 'danger';
-    this.bsModalRef.content.message = 'Erro ao carregar cursos. Tente novamente mais tarde.';
+     this.alertService.showAlertDanger("Erro ao carregar cursos. Tente novamente mais tarde.");
+    // this.bsModalRef = this.modalService.show(AlertModalComponent);
+    // this.bsModalRef.content.type = 'danger';
+    // this.bsModalRef.content.message = 'Erro ao carregar cursos. Tente novamente mais tarde.';
   }
 
 }
