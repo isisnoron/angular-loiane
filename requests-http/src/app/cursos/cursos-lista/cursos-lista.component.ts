f9e6266d09914c6b8cd5617121b08defc0186703
Isis Ribeiro <isisbeatrizrn@gmail.com>
2022-02-28 14:52:49 -0300

    Novo formulário para criar e editar cursos

--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -40,17 +40,17 @@ export class CursosListaComponent implements OnInit {
           return empty();
         })
       );
-    this.service.list()
-      .pipe(
-        catchError(error => empty())
-      )
-      .subscribe(
-        dados => {
-          console.log(dados);
-        }
+    // this.service.list()
+    //   .pipe(
+    //     catchError(error => empty())
+    //   )
+    //   .subscribe(
+    //     dados => {
+    //       console.log(dados);
+    //     }
         // , error => console.error(error),
         // () => console.log('Observable completo!')
-      )
+      // )
   }
 
   handleError() {
