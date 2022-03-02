f3820a780ec5eb48d6cb13586e00cd6fe7f2cf98
Isis Ribeiro <isisbeatrizrn@gmail.com>
2022-03-02 17:09:00 -0300

    HTTP Request com FormData (upload de arquivo)

--- requests-http/src/app/upload-file/upload-file/upload-file.component.ts
+++ requests-http/src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,29 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UploadFileService } from '../upload-file.service';
 
 @Component({
   selector: 'app-upload-file',
   templateUrl: './upload-file.component.html',
-  styleUrls: ['./upload-file.component.scss']
+  styleUrls: ['./upload-file.component.scss'],
 })
 export class UploadFileComponent implements OnInit {
+  files: Set<File>;
+  sub: Subscription[] = [];
 
-  constructor() { }
+  constructor(private service: UploadFileService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  // não é necessário utilizar o código abaixo com bootstrap 5,
+  // porém não consegui fazer aparecer os arquivos selecionados
+  onChange(event: any) {
+    console.log(event);
+
+    const selectedFiles = <FileList>event.srcElement.files;
+    // document.getElementById('customFileLabel').innerHTML = selectedFiles[0].name;
+
+    const fileNames = [];
+    this.files = new Set();
+    for (let i = 0; i < selectedFiles.length; i++) {
+      fileNames.push(selectedFiles[i].name);
+      this.files.add(selectedFiles[i]);
+    }
+    document.getElementById('customFileLabel').innerHTML = fileNames.join(', ');
   }
 
-  // não é necessário utilizar o código abaixo com bootstrap 5, 
-  // porém não consegui fazer aparecer os arquivos selecionados
-  // onChange(event: any) {
-  //   console.log(event);
+  onUpload() {
+    if (this.files && this.files.size > 0) {
+      this.service
+        .upload(this.files, 'http://localhost:8000/upload')
+        .subscribe((response: any) => console.log('Upload Concluído'));
+    }
+  }
 
-  //   const selectedFiles = <FileList>event.srcElement.files;
-  //   // document.getElementById('customFileLabel').innerHTML = selectedFiles[0].name;
+  // não sei se está funcionando, aula 137 loiane pede para não usar o take e sim ngOnDestroy
+  ngOnDestroy() {
+    this.sub.forEach(s => s.unsubscribe());
+    console.log(`${this.files} foi destruído.`);
+  }
 
-  //   const fileNames = [];
-  //   for (let i = 0; i < selectedFiles.length; i++) {
-  //     fileNames.push(selectedFiles[i].name);
-  //   }
-  //   document.getElementById('customFileLabel').innerHTML = fileNames.join(', ');
-  // }
 }
