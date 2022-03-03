85cc01dfd0ccc567e6a787d37d3ef5fa5ddf59ec
Isis Ribeiro <isisbeatrizrn@gmail.com>
2022-03-03 17:01:29 -0300

    Pesquisa/Busca automática com programação reativa

--- requests-http/src/app/reactive-search/lib-search/lib-search.component.ts
+++ requests-http/src/app/reactive-search/lib-search/lib-search.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { map, Observable, tap } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, map, Observable, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-lib-search',
@@ -13,10 +13,28 @@ export class LibSearchComponent implements OnInit {
   readonly SEARCH_URL = 'https://api.cdnjs.com/libraries';
   results$: Observable<any>;
   total: number;
+  readonly FIELDS = 'name,description,version,homepage';
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.results$ = this.queryField.valueChanges
+    .pipe(
+      map(value => value.trim()),
+      filter(value => value.length > 1),
+      debounceTime(200),
+      distinctUntilChanged(),
+      tap(value => console.log(value)),
+      switchMap(value => this.http.get(this.SEARCH_URL, {
+        params: {
+          search: value,
+          fields: this.FIELDS
+        }
+      })),
+      tap((res: any) => this.total = res.total),
+      map((res: any) => res.results)
+    );
+  }
 
   onSearch() {
     console.log(this.queryField.value);
@@ -37,5 +55,5 @@ export class LibSearchComponent implements OnInit {
         map((res: any) => res.results)
       );
     }
-  }
+  } 
 }
