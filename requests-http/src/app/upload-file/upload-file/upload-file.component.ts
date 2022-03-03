a7bd144febbe8fbad345d555ecd1eabc943591c2
Isis Ribeiro <isisbeatrizrn@gmail.com>
2022-03-03 11:31:06 -0300

    Criando operador RXJS customizado (para upload)

--- requests-http/src/app/upload-file/upload-file/upload-file.component.ts
+++ requests-http/src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,6 +1,7 @@
 import { HttpEvent, HttpEventType } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { filter, Subscription } from 'rxjs';
+import { filterResponse, uploadProgress } from 'src/app/shared/rxjs-operators';
 import { environment } from 'src/environments/environment';
 import { UploadFileService } from '../upload-file.service';
 
@@ -40,18 +41,24 @@ export class UploadFileComponent implements OnInit {
   onUpload() {
     if (this.files && this.files.size > 0) {
       this.service.upload(this.files, environment.BASE_URL + '/upload')
-      .subscribe((event: HttpEvent<Object>) => {
-        // HttpEventType
-         console.log(event);
-         if (event.type === HttpEventType.Response) {
-           console.log('Upload Concluído');
-         } else if (event.type === HttpEventType.UploadProgress) {
-           const percentDone = Math.round((event.loaded * 100) / event.total);
-           console.log('Progresso', percentDone);
-           this.progress = percentDone;
+      .pipe(
+        uploadProgress(progress => {
+          console.log(progress);
+          this.progress = progress;
+        }),
+        filterResponse())
+        .subscribe(response => console.log('Upload Concluído'))
+      // .subscribe((event: HttpEvent<Object>) => {
+      //    //console.log(event);
+      //    if (event.type === HttpEventType.Response) {
+      //      console.log('Upload Concluído');
+      //    } else if (event.type === HttpEventType.UploadProgress) {
+      //      const percentDone = Math.round((event.loaded * 100) / event.total);
+      //      //console.log('Progresso', percentDone);
+      //      this.progress = percentDone;
            
-         }
-       } );
+      //    }
+      //  } );
     }
   }
 
